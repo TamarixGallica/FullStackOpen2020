422af0f7d171eaf69aaca94904fca38a584174f5
Jani Havukainen <github@posti.titityy.net>
2020-03-17 08:18:42 +0200

    Tehtävä 9.1

---
+++ typescript/multiplier.ts
@@ -0,0 +1,20 @@
+type Operation = 'multiply' | 'add' | 'divide';
+
+type CalculatorFn = number;
+
+const calculator = (a: number, b: number, op: Operation) => {
+    if (op === 'multiply') {
+        return a * b;
+    } else if (op === 'add') {
+        return a + b;
+    } else if (op === 'divide') {
+        if (b === 0) return 'Can\'t divide by 0!';
+        return a / b;
+    }
+}
+
+const multiplicator = (a: number, b: number, printText: string) => {
+    console.log(printText, a * b);
+}
+
+multiplicator(2, 4, 'Multiplied numbers 2 and 4, the result is:');
